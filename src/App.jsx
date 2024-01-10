256e3df5141a6f9f33d3ead93c4137378aa40ded
Tayomide <70061548+Tayomide@users.noreply.github.com>
2024-01-10 01:12:37 -0600

    Reattempt at font loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 import { Routes, Route } from 'react-router-dom'
 import { Home } from './pages/Home'
-import { Navbar } from './components/Navbar'
 
 function App() {
 
